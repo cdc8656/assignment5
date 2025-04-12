248452853d76ee7d44101db4667ae9ca7c18f0c0
cdc8656 <145940588+cdc8656@users.noreply.github.com>
2025-04-12 17:46:31 +0800

    Update index.js
    naming

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import AirlineRoutes from "../components/assignment5/assignment5_student";
+import AirlineRoutes from "../components/assignment5/assignment5_Christopher_Chen";
 
 export default function Home() {
   return <AirlineRoutes />;
